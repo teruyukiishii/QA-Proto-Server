4cddaa6823c3744de62e24d91c2a3033a1999053
TeruyukiIshii <TeruyukiIshii@gmail.com>
2017-11-05 16:26:49 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.delete_all()
+Question.create(:content => '質問です')
+Question.create(:content => 'もう一回質問します')
+Question.create(:content => 'さらに質問します')
