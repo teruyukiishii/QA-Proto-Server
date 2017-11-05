3fa407b1d24efb74fa698181d94b7b021e076d6d
TeruyukiIshii <TeruyukiIshii@gmail.com>
2017-11-05 18:13:30 +0900

    api cross domain

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module QAProtoServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
