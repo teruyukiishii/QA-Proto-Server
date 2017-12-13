6d9c2ea36c0b0e8c740704fa2952ef3de77fe50f
TeruyukiIshii <TeruyukiIshii@gmail.com>
2017-12-13 20:35:05 +0900

    omniauth-stripe

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
    OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys!
    provider :facebook, OAUTH_CONFIG[:facebook]['key'], OAUTH_CONFIG[:facebook]['secret'], scope: '' 
+   provider :stripe,   OAUTH_CONFIG[:stripe]['key'], OAUTH_CONFIG[:stripe]['secret']
 end
